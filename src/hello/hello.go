20b135a459421be0baec59469ac86c286f8e2c7d
Marcelo Ramalho Oliveira <marcelo.ramalho-oliveira@outlook.com>
2022-12-31 15:31:26 -0300

    exercicio funcoes

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,22 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+
+	default:
+		fmt.Println("Nao conheço esse comando")
+		os.Exit(-1)
+	}
+
+}
+
+func exibeIntroducao() {
 	nome := "Marcelo"
 	versao := "1.1"
-
 	fmt.Println("Ola sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O valor da variavel comando é: ", comando)
-
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O valor da variavel comando é: ", comandoLido)
+	return comandoLido
 }
